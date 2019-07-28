7c030d2dc73879296135ee2989f63983eb73053a
Jair Junior <jairbj@gmail.com>
2019-07-28 14:57:20 -0300

    Add script to install packages to EmulationStation

---
+++ emulation_station_packages.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y libsdl2-dev libboost-system-dev libboost-filesystem-dev libboost-date-time-dev libboost-locale-dev libfreeimage-dev libfreetype6-dev libeigen3-dev libcurl4-openssl-dev libasound2-dev libgl1-mesa-dev build-essential cmake git
